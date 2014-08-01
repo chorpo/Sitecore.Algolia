65b5151ef01e9b0b5c7b98ad3d8405fc50b34661
Dmitry Harnitski <dmitry@brainjocks.com>
2014-07-31 23:14:23 -0400

    http://www.sitecore.net/deutsch/Community/Technical-Blogs/Getting-to-Know-Sitecore/Posts/2013/04/Sitecore-7-Search-Provider-Part-1-Manually-Triggered-Indexing.aspx - Update Context step 7

---
+++ Algolia.SitecoreProvider/IndexOperations.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using Sitecore.ContentSearch;
+using Sitecore.ContentSearch.Linq.Common;
+using Sitecore.Data.Items;
+
+namespace Algolia.SitecoreProvider
+{
+    public class IndexOperations : IIndexOperations
+    {
+        public void Update(IIndexable indexable, IProviderUpdateContext context,
+            ProviderIndexConfiguration indexConfiguration)
+        {
+            var doc = GetDocument(indexable);
+            context.UpdateDocument(doc, null, (IExecutionContext) null);
+        }
+
+        public void Delete(IIndexable indexable, IProviderUpdateContext context)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(IIndexableId id, IProviderUpdateContext context)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(IIndexableUniqueId indexableUniqueId, IProviderUpdateContext context)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Add(IIndexable indexable, IProviderUpdateContext context,
+            ProviderIndexConfiguration indexConfiguration)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected virtual JObject GetDocument(IIndexable indexable)
+        {
+            var item = (Item) (indexable as SitecoreIndexableItem);
+
+            //var tags = new List<string>();
+            //tags.Add("image");
+
+            //if (!string.IsNullOrEmpty(item.GpId))
+            //    tags.Add("gpId_" + item.GpId);
+
+            //tags.Add("owner_" + item.UserId);
+            //tags.Add("tripId_" + item.TripId);
+            //foreach (var connectionId in item.UsersCanSee)
+            //{
+            //    tags.Add("connection_" + connectionId);
+            //}
+
+            dynamic w = new ExpandoObject();
+
+            w.name = item.Name;
+            w.path = item.Paths.Path;
+
+            return SerializeExpandoObject(w);
+        }
+
+        private static JObject SerializeExpandoObject(dynamic data)
+        {
+            var dict = (IDictionary<string, object>) data;
+            for (int i = dict.Keys.Count - 1; i >= 0; i--)
+            {
+                var key = dict.Keys.ElementAt(i);
+                if (dict[key] == null)
+                {
+                    ((IDictionary<String, Object>) data).Remove(key);
+                }
+            }
+            var str = JsonConvert.SerializeObject(data,
+                new JsonSerializerSettings
+                {
+                    NullValueHandling = NullValueHandling.Ignore,
+                    StringEscapeHandling = StringEscapeHandling.Default,
+                });
+            var jobj = JObject.Parse(str);
+            return jobj;
+        }
+    }
+}
+
---
+++ Algolia.SitecoreProvider/ProviderUpdateContext.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json.Linq;
+using Sitecore.ContentSearch;
+using Sitecore.ContentSearch.Linq.Common;
+
+namespace Algolia.SitecoreProvider
+{
+    public class ProviderUpdateContext: IProviderUpdateContext
+    {
+        private readonly ISearchIndex _index;
+        private List<JObject> _updateDocs;
+        
+        public ProviderUpdateContext(ISearchIndex index)
+        {
+            _index = index;
+            _updateDocs = new List<JObject>();
+        }
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Commit()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Optimize()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void AddDocument(object itemToAdd, IExecutionContext executionContext)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void AddDocument(object itemToAdd, params IExecutionContext[] executionContexts)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateDocument(object itemToUpdate, object criteriaForUpdate, IExecutionContext executionContext)
+        {
+            var doc = itemToUpdate as JObject;
+            _updateDocs.Add(doc);
+        }
+
+        public void UpdateDocument(object itemToUpdate, object criteriaForUpdate, params IExecutionContext[] executionContexts)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(IIndexableUniqueId id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(IIndexableId id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool IsParallel { get; private set; }
+        public ParallelOptions ParallelOptions { get; private set; }
+        public ISearchIndex Index
+        {
+            get { return _index; }
+        }
+        public ICommitPolicyExecutor CommitPolicyExecutor { get; private set; }
+    }
+}
