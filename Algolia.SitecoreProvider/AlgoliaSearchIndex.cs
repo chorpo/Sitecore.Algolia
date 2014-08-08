c0b4903449c35aba4d242f67a6fd31ed7825443e
Dmitry Harnitski <dmitry@brainjocks.com>
2014-08-08 16:05:17 -0400

    Part 1 Done

--- Algolia.SitecoreProvider/AlgoliaSearchIndex.cs
+++ Algolia.SitecoreProvider/AlgoliaSearchIndex.cs
@@ -17,11 +17,16 @@ namespace Algolia.SitecoreProvider
     {
         private readonly AlgoliaConfig _config;
 
-        public AlgoliaSearchIndex(string applicationId, string fullApiKey, string indexName)
+        public AlgoliaSearchIndex(string name, string applicationId, string fullApiKey, string indexName, IIndexPropertyStore propertyStore)
         {
+            if (propertyStore == null) throw new ArgumentNullException("propertyStore");
+            if (string.IsNullOrWhiteSpace(name)) throw new ArgumentOutOfRangeException("name");
             if (string.IsNullOrWhiteSpace(applicationId)) throw new ArgumentOutOfRangeException("applicationId");
             if (string.IsNullOrWhiteSpace(fullApiKey)) throw new ArgumentOutOfRangeException("fullApiKey");
             if (string.IsNullOrWhiteSpace(indexName)) throw new ArgumentOutOfRangeException("indexName");
+
+            Name = name;
+            PropertyStore = propertyStore;
             
             var config = new AlgoliaConfig
             {
--- Algolia.SitecoreProviderTests/Integrations/AddItemsIntegrations.cs
+++ Algolia.SitecoreProviderTests/Integrations/AddItemsIntegrations.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 using Algolia.SitecoreProvider;
 using NUnit.Framework;
 using Sitecore.ContentSearch;
+using Sitecore.ContentSearch.Maintenance;
 using Sitecore.Data;
 using Sitecore.Data.Items;
 using Sitecore.FakeDb;
@@ -26,14 +27,14 @@ namespace Algolia.SitecoreProviderTests.Integrations
         }
 
         [Test]
-        public void AddItemsIntegration()
+        public void AddItemsIntegration()  
         {
             // arrange
             using (var db = new Db {_source})
             {
                 var item = db.GetItem("/sitecore/content/source");
                 var indexable = new SitecoreIndexableItem(item);
-                var index = new AlgoliaSearchIndex("3Q92VD0BCR", "ae8283da0a1342b03749ed1355d9d4a7", "sitecore");
+                var index = new AlgoliaSearchIndex("algolia_master_index", "3Q92VD0BCR", "ae8283da0a1342b03749ed1355d9d4a7", "sitecore", new NullPropertyStore());
                 var context = index.CreateUpdateContext();
                 
                 var operations = new AlgoliaIndexOperations();
