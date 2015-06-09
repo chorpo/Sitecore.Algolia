f51f8559bc491a67e9ecc5ed83baff4ad9b13bec
Dmitry Harnitski <dmitry@brainjocks.com>
2015-06-09 17:23:59 -0400

    testing Configuration Load for Index - test failed

---
+++ Score.ContentSearch.Algolia.Tests/Configuration/AlgoliaSearchIndexTests.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace Score.ContentSearch.Algolia.Tests.Configuration
+{
+    [TestFixture]
+    public class AlgoliaSearchIndexTests
+    {
+        [Test]
+        public void ShouldCreateFromConfig()
+        {
+            //Arrange
+            string xmlPath = @"Configuration\Algolia.Search.config";
+            var xmlDoc = new System.Xml.XmlDocument();
+            xmlDoc.Load(xmlPath);
+            //var firstElement = (System.Xml.XmlElement)xmlDoc.DocumentElement.FirstChild;
+
+            XmlElement root = xmlDoc.DocumentElement;
+            var configNode = root.SelectSingleNode("//configuration//sitecore//contentSearch//configuration//indexes");
+
+            configNode.Should().NotBeNull();
+
+            //Act
+            var index = Sitecore.Configuration.Factory.CreateObject<AlgoliaSearchIndex>(configNode);
+
+
+            //Assert
+        }
+    }
+}
