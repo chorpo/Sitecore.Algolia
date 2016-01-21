7888e76ea230eca6e676dba24df151ad55c523fd
Dmitry Harnitski <dmitry@brainjocks.com>
2016-01-21 13:00:29 -0500

    add "cdTargetHostName" site attribute to generate correct URLs in CM server

--- Score.ContentSearch.Algolia/ComputedFields/CurrentItemLinkField.cs
+++ Score.ContentSearch.Algolia/ComputedFields/CurrentItemLinkField.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.Specialized;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,6 +9,8 @@ using Sitecore.ContentSearch;
 using Sitecore.ContentSearch.ComputedFields;
 using Sitecore.Links;
 using Sitecore.Sites;
+using Sitecore.Web;
+using Sitecore.Web.UI.WebControls;
 
 namespace Score.ContentSearch.Algolia.ComputedFields
 {
@@ -31,13 +34,39 @@ namespace Score.ContentSearch.Algolia.ComputedFields
             return url;
         }
 
-        protected virtual void AdjustOptions(SitecoreIndexableItem item) 
+        protected virtual void AdjustOptions(SitecoreIndexableItem item)
         {
             if (!string.IsNullOrEmpty(Site))
             {
                 var siteContext = SiteContext.GetSite(Site);
+                if (siteContext == null)
+                    throw new Exception($"Site {Site} cannot be reached");
+
+                //we typicaly generate index on CM but site URLs in CM will not work for CD
+                //replacing "targetHostName" is not good solution because it breaks PE in CM
+                //"cdTargetHostName" site argument should solve that issue. Index uses it instead of targetHostName and PE continue using "targetHostName" 
+                var cmTargetHostName = siteContext.Properties["cdTargetHostName"];
+                if (!String.IsNullOrWhiteSpace(cmTargetHostName))
+                {
+                    var props = new Sitecore.Collections.StringDictionary(ToDictionary(siteContext.SiteInfo.Properties));
+                    props["targetHostName"] = cmTargetHostName;
+
+                    var siteInfo = new SiteInfo(props);
+                    siteContext = new SiteContext(siteInfo);
+                }
+
                 UrlOptions.Site = siteContext;
             }
+        }
+
+        private static IDictionary<string, string> ToDictionary(NameValueCollection col)
+        {
+            IDictionary<string, string> dict = new Dictionary<string, string>();
+            foreach (var k in col.AllKeys)
+            {
+                dict.Add(k, col[k]);
+            }
+            return dict;
         }
 
         protected virtual string PostProcessUrl(string url)
